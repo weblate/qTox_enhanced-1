40fe0388bc095a984b7df4a5c7a7c1bf349ba853
iphydf <iphydf@users.noreply.github.com>
2024-11-14 21:39:22 +0000

    cleanup: Move all license headers in C++ source files to SPDX.

--- src/platform/timer_osx.cpp
+++ src/platform/timer_osx.cpp
@@ -1,27 +1,13 @@
-/*
-    Pidgin is the legal property of its developers, whose names are too numerous
-    to list here.  Please refer to the COPYRIGHT file distributed with this
-    source distribution (which can be found at
-    <https://hg.pidgin.im/pidgin/main/file/13e4ae613a6a/COPYRIGHT> ).
-
-    Copyright © 2006 by Richard Laager
-    Copyright © 2014-2019 by The qTox Project Contributors
-
-    This file is part of qTox, a Qt-based graphical interface for Tox.
-
-    qTox is libre software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    qTox is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with qTox.  If not, see <http://www.gnu.org/licenses/>.
-*/
+/* SPDX-License-Identifier: GPL-3.0-or-later
+ * Copyright © 2006 by Richard Laager
+ * Copyright © 2014-2019 by The qTox Project Contributors
+ * Copyright © 2024 The TokTok team.
+ *
+ * Pidgin is the legal property of its developers, whose names are too numerous
+ * to list here.  Please refer to the COPYRIGHT file distributed with this
+ * source distribution (which can be found at
+ * <https://hg.pidgin.im/pidgin/main/file/13e4ae613a6a/COPYRIGHT> ).
+ */
 
 #include "src/platform/timer.h"
 #include <CoreFoundation/CoreFoundation.h>
