8cd1b73e7b01d184bd8cdf9d9318a38764f76ba6
iphydf <iphydf@users.noreply.github.com>
2024-11-22 23:25:00 +0000

    chore: Build windows debug builds with a console.
    Just so we can run it and observe its stdout/stderr. We do have a debug
    log, but if it crashes before writing that log, we can't see it.

--- windows/cross-compile/build.sh
+++ windows/cross-compile/build.sh
@@ -1,32 +1,13 @@
 #!/usr/bin/env bash
 
-# MIT License
-#
-# Copyright (c) 2017-2021 Maxim Biro <nurupo.contributions@gmail.com>
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
+# SPDX-License-Identifier: MIT
+# Copyright © 2017-2021 Maxim Biro <nurupo.contributions@gmail.com>
+# Copyright © 2024 The TokTok team.
 
 # Known issues:
 # - Doesn't build qTox updater, because it wasn't ported to cmake yet and
 #   because it requires static Qt, which means we'd need to build Qt twice, and
 #   building Qt takes really long time.
-
 
 set -euo pipefail
 
@@ -48,7 +29,6 @@ QTOX_PREFIX_DIR="$(realpath install-prefix)"
 readonly QTOX_PREFIX_DIR
 QTOX_PACKAGE_DIR="$(realpath package-prefix)"
 readonly QTOX_PACKAGE_DIR
-
 
 if [ -z "$ARCH" ]
 then
@@ -106,6 +86,7 @@ then
         -DSTRICT_OPTIONS=ON \
         -DTEST_CROSSCOMPILING_EMULATOR=wine \
         -GNinja \
+        -DCMAKE_EXE_LINKER_FLAGS="-mconsole" \
         "$QTOX_SRC_DIR"
 fi
 
