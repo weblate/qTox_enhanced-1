87f9de08a0db7f7a1cfa88c643eb5f1718c99917
iphydf <iphydf@users.noreply.github.com>
2024-12-25 00:37:13 +0000

    test: Update loginscreen UI test golden image.

--- test/widget/loginscreen_test.cpp
+++ test/widget/loginscreen_test.cpp
@@ -1,8 +1,10 @@
 #include "src/widget/loginscreen.h"
 
+#include <QDebug>
+#include <QProcessEnvironment>
 #include <QTest>
 
-#include "src/persistence/settings.h"
+#include "src/persistence/paths.h"
 #include "src/widget/style.h"
 
 class TestLoginScreen : public QObject
@@ -21,6 +23,14 @@ void TestLoginScreen::testLoginScreen()
     LoginScreen loginScreen(paths, style, themeColor, profileName);
 
     const QPixmap actual = loginScreen.grab();
+    const auto env = QProcessEnvironment::systemEnvironment();
+    if (env.contains("BUILD_WORKSPACE_DIRECTORY")) {
+        const QString actualPath = QStringLiteral("%1/%2")
+                                       .arg(env.value("BUILD_WORKSPACE_DIRECTORY"))
+                                       .arg("qtox/test/resources/images/loginscreen.png");
+        qDebug() << "Saving actual image to" << actualPath;
+        actual.save(actualPath);
+    }
     const QPixmap expected = QPixmap(":/test/images/loginscreen.png");
     QCOMPARE(actual, expected);
 }
