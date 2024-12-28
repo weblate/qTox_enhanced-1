aba18f10528d372c5c6e600f050d4b5e13f64a31
iphydf <iphydf@users.noreply.github.com>
2024-12-28 14:35:48 +0000

    chore: Install homebrew bash v5.
    macOS comes with bash v3, which doesn't do arrays. We can't comply with
    restyled/codacy requirements without arrays.

--- macos/update-plist-version.sh
+++ macos/update-plist-version.sh
@@ -1,19 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-#    Copyright © 2016-2019 The qTox Project Contributors
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright © 2016-2019 by The qTox Project Contributors
+# Copyright © 2024 The TokTok team
 
 # script to change qTox version in `Info.plist` file to the supplied one
 #
@@ -29,7 +18,7 @@
 #
 # $version has to be composed of at least one number/dot
 
-set -eu -o pipefail
+set -euo pipefail
 
 # update version in `Info.plist` file to supplied one after the right lines
 update_version() {
