e47356681c126dffe9f07fbfa4ee9d441446c230
iphydf <iphydf@users.noreply.github.com>
2024-12-31 17:14:17 +0000

    fix(Security): Harden the persistence/serialization functions.
    These ought to be used with safe data, but who knows whether they
    actually are. Now, these functions will never fatally misbehave.

--- src/persistence/serialize.h
+++ src/persistence/serialize.h
@@ -3,17 +3,9 @@
  * Copyright © 2024 The TokTok team.
  */
 
-
 #pragma once
 
 #include <QByteArray>
-#include <QString>
-#include <cstdint>
 
-QString dataToString(QByteArray data);
-uint64_t dataToUint64(const QByteArray& data);
 int dataToVInt(const QByteArray& data);
-size_t dataToVUint(const QByteArray& data);
-unsigned getVUint32Size(QByteArray data);
 QByteArray vintToData(int num);
-QByteArray vuintToData(size_t num);
