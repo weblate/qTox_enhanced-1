iphydf/qTox

cleanup: Rename all .css files to .qss.
This way, editor syntax highlighting is happy. These files are not
actual CSS syntax.

src/widget/loginscreen.cpp
@@ -53,7 +53,7 @@ LoginScreen::LoginScreen(Settings& settings_, Style& style, const QString& initi
     connect(ui->importButton, &QPushButton::clicked, this, &LoginScreen::onImportProfile);
 
     reset(initialProfileName);
-    setStyleSheet(style.getStylesheet("loginScreen/loginScreen.css", settings));
+    setStyleSheet(style.getStylesheet("loginScreen/loginScreen.qss", settings));
 
     retranslateUi();
     Translator::registerHandler(std::bind(&LoginScreen::retranslateUi, this), this);

weblate/qTox_enhanced-1

src/widget/loginscreen.cpp
@@ -53,7 +53,7 @@ LoginScreen::LoginScreen(Paths& paths_, Style& style, int themeColor,
     connect(ui->importButton, &QPushButton::clicked, this, &LoginScreen::onImportProfile);
 
     reset(initialProfileName);
-    setStyleSheet(style.getStylesheet("loginScreen/loginScreen.css", themeColor));
+    setStyleSheet(style.getStylesheet("loginScreen/loginScreen.qss", themeColor));
 
     retranslateUi();
     Translator::registerHandler(std::bind(&LoginScreen::retranslateUi, this), this);