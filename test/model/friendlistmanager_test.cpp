TokTok/qTox

cleanup: Use `= default` instead of `{}` for default ctors.
Also remove redundant `virtual` when functions are already marked as
`override`. Also use `nullptr` more.

```sh
run-clang-tidy -p _build -fix \
  $(find . -name "*.h" -or -name "*.cpp" -or -name "*.c" | grep -v "/_build/") \
  -checks="-*,modernize-use-equals-*,modernize-use-nullptr,modernize-use-override"
```

test/model/friendlistmanager_test.cpp
@@ -25,7 +25,7 @@ public:
     {
     }
 
-    ~MockFriend();
+    ~MockFriend() override;
 
     bool isFriend() const override
     {
@@ -84,7 +84,7 @@ public:
     {
     }
 
-    ~MockGroup();
+    ~MockGroup() override;
 
     bool isFriend() const override
     {

test/model/notificationgenerator_test.cpp
@@ -15,66 +15,66 @@
 namespace {
 class MockNotificationSettings : public INotificationSettings
 {
-    virtual bool getNotify() const override
+    bool getNotify() const override
     {
         return true;
     }
 
-    virtual void setNotify(bool newValue) override
+    void setNotify(bool newValue) override
     {
         std::ignore = newValue;
     }
 
-    virtual bool getShowWindow() const override
+    bool getShowWindow() const override
     {
         return true;
     }
-    virtual void setShowWindow(bool newValue) override
+    void setShowWindow(bool newValue) override
     {
         std::ignore = newValue;
     }
 
-    virtual bool getDesktopNotify() const override
+    bool getDesktopNotify() const override
     {
         return true;
     }
-    virtual void setDesktopNotify(bool enabled) override
+    void setDesktopNotify(bool enabled) override
     {
         std::ignore = enabled;
     }
 
-    virtual bool getNotifySound() const override
+    bool getNotifySound() const override
     {
         return true;
     }
-    virtual void setNotifySound(bool newValue) override
+    void setNotifySound(bool newValue) override
     {
         std::ignore = newValue;
     }
 
-    virtual bool getNotifyHide() const override
+    bool getNotifyHide() const override
     {
         return notifyHide;
     }
-    virtual void setNotifyHide(bool newValue) override
+    void setNotifyHide(bool newValue) override
     {
         notifyHide = newValue;
     }
 
-    virtual bool getBusySound() const override
+    bool getBusySound() const override
     {
         return true;
     }
-    virtual void setBusySound(bool newValue) override
+    void setBusySound(bool newValue) override
     {
         std::ignore = newValue;
     }
 
-    virtual bool getGroupAlwaysNotify() const override
+    bool getGroupAlwaysNotify() const override
     {
         return true;
     }
-    virtual void setGroupAlwaysNotify(bool newValue) override
+    void setGroupAlwaysNotify(bool newValue) override
     {
         std::ignore = newValue;
     }
@@ -169,7 +169,8 @@ void TestNotificationGenerator::testNotificationClear()
 
 void TestNotificationGenerator::testGroupMessage()
 {
-    Group g(0, GroupId(0), "groupName", false, "selfName", *groupQuery, *coreIdHandler, *friendList);
+    Group g(0, GroupId(nullptr), "groupName", false, "selfName", *groupQuery, *coreIdHandler,
+            *friendList);
     auto sender = groupQuery->getGroupPeerPk(0, 0);
     g.updateUsername(sender, "sender1");
 
@@ -180,7 +181,8 @@ void TestNotificationGenerator::testGroupMessage()
 
 void TestNotificationGenerator::testMultipleGroupMessages()
 {
-    Group g(0, GroupId(0), "groupName", false, "selfName", *groupQuery, *coreIdHandler, *friendList);
+    Group g(0, GroupId(nullptr), "groupName", false, "selfName", *groupQuery, *coreIdHandler,
+            *friendList);
 
     auto sender = groupQuery->getGroupPeerPk(0, 0);
     g.updateUsername(sender, "sender1");
@@ -350,7 +352,8 @@ void TestNotificationGenerator::testSimpleFileTransfer()
 
 void TestNotificationGenerator::testSimpleGroupMessage()
 {
-    Group g(0, GroupId(0), "groupName", false, "selfName", *groupQuery, *coreIdHandler, *friendList);
+    Group g(0, GroupId(nullptr), "groupName", false, "selfName", *groupQuery, *coreIdHandler,
+            *friendList);
     auto sender = groupQuery->getGroupPeerPk(0, 0);
     g.updateUsername(sender, "sender1");
 

weblate/qTox_enhanced-1

test/model/friendlistmanager_test.cpp
@@ -25,7 +25,7 @@ public:
     {
     }
 
-    ~MockFriend();
+    ~MockFriend() override;
 
     bool isFriend() const override
     {
@@ -84,7 +84,7 @@ public:
     {
     }
 
-    ~MockConference();
+    ~MockConference() override;
 
     bool isFriend() const override
     {

test/model/notificationgenerator_test.cpp
@@ -15,75 +15,75 @@
 namespace {
 class MockNotificationSettings : public INotificationSettings
 {
-    virtual bool getNotify() const override
+    bool getNotify() const override
     {
         return true;
     }
 
-    virtual void setNotify(bool newValue) override
+    void setNotify(bool newValue) override
     {
         std::ignore = newValue;
     }
 
-    virtual bool getShowWindow() const override
+    bool getShowWindow() const override
     {
         return true;
     }
-    virtual void setShowWindow(bool newValue) override
+    void setShowWindow(bool newValue) override
     {
         std::ignore = newValue;
     }
 
-    virtual bool getDesktopNotify() const override
+    bool getDesktopNotify() const override
     {
         return true;
     }
-    virtual void setDesktopNotify(bool newValue) override
+    void setDesktopNotify(bool enabled) override
     {
-        std::ignore = newValue;
+        std::ignore = enabled;
     }
 
-    virtual bool getNotifySystemBackend() const override
+    bool getNotifySystemBackend() const override
     {
         return true;
     }
-    virtual void setNotifySystemBackend(bool newValue) override
+    void setNotifySystemBackend(bool newValue) override
     {
         std::ignore = newValue;
     }
 
-    virtual bool getNotifySound() const override
+    bool getNotifySound() const override
     {
         return true;
     }
-    virtual void setNotifySound(bool newValue) override
+    void setNotifySound(bool newValue) override
     {
         std::ignore = newValue;
     }
 
-    virtual bool getNotifyHide() const override
+    bool getNotifyHide() const override
     {
         return notifyHide;
     }
-    virtual void setNotifyHide(bool newValue) override
+    void setNotifyHide(bool newValue) override
     {
         notifyHide = newValue;
     }
 
-    virtual bool getBusySound() const override
+    bool getBusySound() const override
     {
         return true;
     }
-    virtual void setBusySound(bool newValue) override
+    void setBusySound(bool newValue) override
     {
         std::ignore = newValue;
     }
 
-    virtual bool getConferenceAlwaysNotify() const override
+    bool getConferenceAlwaysNotify() const override
     {
         return true;
     }
-    virtual void setConferenceAlwaysNotify(bool newValue) override
+    void setConferenceAlwaysNotify(bool newValue) override
     {
         std::ignore = newValue;
     }
@@ -178,7 +178,7 @@ void TestNotificationGenerator::testNotificationClear()
 
 void TestNotificationGenerator::testConferenceMessage()
 {
-    Conference g(0, ConferenceId(0), "conferenceName", false, "selfName", *conferenceQuery,
+    Conference g(0, ConferenceId(nullptr), "conferenceName", false, "selfName", *conferenceQuery,
                  *coreIdHandler, *friendList);
     auto sender = conferenceQuery->getConferencePeerPk(0, 0);
     g.updateUsername(sender, "sender1");
@@ -190,7 +190,7 @@ void TestNotificationGenerator::testConferenceMessage()
 
 void TestNotificationGenerator::testMultipleConferenceMessages()
 {
-    Conference g(0, ConferenceId(0), "conferenceName", false, "selfName", *conferenceQuery,
+    Conference g(0, ConferenceId(nullptr), "conferenceName", false, "selfName", *conferenceQuery,
                  *coreIdHandler, *friendList);
 
     auto sender = conferenceQuery->getConferencePeerPk(0, 0);
@@ -361,7 +361,7 @@ void TestNotificationGenerator::testSimpleFileTransfer()
 
 void TestNotificationGenerator::testSimpleConferenceMessage()
 {
-    Conference g(0, ConferenceId(0), "conferenceName", false, "selfName", *conferenceQuery,
+    Conference g(0, ConferenceId(nullptr), "conferenceName", false, "selfName", *conferenceQuery,
                  *coreIdHandler, *friendList);
     auto sender = conferenceQuery->getConferencePeerPk(0, 0);
     g.updateUsername(sender, "sender1");