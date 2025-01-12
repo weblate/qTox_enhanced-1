ce93798c060bfabfaad7a97a08882ef12622d0b6
Robin Linden <dev@robinlinden.eu>
2025-01-13 00:24:13 +0100

    chore: Point the contributor-list link at the one GitHub generates
    The old qTox solution hasn't been updated since 2022, and we probably
    don't want to take over maintenance for it when GitHub already generates
    this information for us.

--- src/widget/form/settings/aboutform.cpp
+++ src/widget/form/settings/aboutform.cpp
@@ -130,7 +130,7 @@ void AboutForm::replaceVersions()
             .arg(tr("Original author: %1").arg(createLink("https://github.com/tux3", "tux3")),
                  tr("See a full list of %1 at Github",
                     "`%1` is replaced with translation of word `contributors`")
-                     .arg(createLink("https://qtox.github.io/gitstats/authors.html",
+                     .arg(createLink("https://github.com/TokTok/qTox/graphs/contributors",
                                      tr("contributors", "Replaces `%1` in `See a full list of…`"))),
                  tr("This version of qTox is being maintained by the TokTok team "
                     "following the archiving of the original qTox project."));
