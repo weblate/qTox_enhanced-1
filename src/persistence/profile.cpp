iphydf/qTox

cleanup: Avoid multiple declarations per statement.
```sh
run-clang-tidy -p _build -fix $(find . -name "*.h" -or -name "*.cpp" \
-or -name "*.c" | grep -v "/_build/") \
-checks="-*,readability-isolate-declaration"
```

src/persistence/profile.cpp
@@ -409,7 +409,8 @@ QStringList Profile::getFilesByExt(QString extension, Settings& settings)
 const QStringList Profile::getAllProfileNames(Settings& settings)
 {
     profiles.clear();
-    QStringList toxfiles = getFilesByExt("tox", settings), inifiles = getFilesByExt("ini", settings);
+    QStringList toxfiles = getFilesByExt("tox", settings);
+    QStringList inifiles = getFilesByExt("ini", settings);
     for (const QString& toxfile : toxfiles) {
         if (!inifiles.contains(toxfile)) {
             settings.createPersonal(toxfile);
@@ -895,7 +896,8 @@ QStringList Profile::remove()
  */
 bool Profile::rename(QString newName)
 {
-    QString path = paths.getSettingsDirPath() + name, newPath = paths.getSettingsDirPath() + newName;
+    QString path = paths.getSettingsDirPath() + name;
+    QString newPath = paths.getSettingsDirPath() + newName;
 
     if (!ProfileLocker::lock(newName, paths)) {
         return false;

weblate/qTox_enhanced-1

src/persistence/profile.cpp
@@ -408,7 +408,8 @@ QStringList Profile::getFilesByExt(QString extension, Paths& paths)
 const QStringList Profile::getAllProfileNames(Paths& paths)
 {
     profiles.clear();
-    QStringList toxFiles = getFilesByExt("tox", paths), iniFiles = getFilesByExt("ini", paths);
+    QStringList toxFiles = getFilesByExt("tox", paths);
+    QStringList iniFiles = getFilesByExt("ini", paths);
     for (const QString& toxFile : toxFiles) {
         if (!iniFiles.contains(toxFile)) {
             Settings::createPersonal(paths, toxFile);
@@ -892,7 +893,8 @@ QStringList Profile::remove()
  */
 bool Profile::rename(QString newName)
 {
-    QString path = paths.getSettingsDirPath() + name, newPath = paths.getSettingsDirPath() + newName;
+    QString path = paths.getSettingsDirPath() + name;
+    QString newPath = paths.getSettingsDirPath() + newName;
 
     if (!ProfileLocker::lock(newName, paths)) {
         return false;